1d829907e0f636217469c77698bbc8a96855874e
harowosegbe <48373214+harowosegbe@users.noreply.github.com>
2023-03-10 12:16:41 +0100

    feat: updated comment

--- admin/views/tutorial.php
+++ admin/views/tutorial.php
@@ -1,19 +1,12 @@
 <?php
 
 /**
- * Provide a admin area view for the plugin
+ * Provide a Tutorial area view for the plugin
  *
- * This file is used to markup the admin-facing aspects of the plugin.
+ * This file is used to markup the tutorial page.
  *
- * @link       http://example.com
- * @since      1.0.0
- *
- * @package    Plugin_Name
- * @subpackage Plugin_Name/admin/partials
  */
 ?>
-
-<!-- This file should primarily consist of HTML with a little bit of PHP. -->
 
 <div class="wrap swiftxr-wrap">
 
